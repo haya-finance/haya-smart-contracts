3c304459944a373b776f1ec47660eb7a6014f130
Dev <FullStackBuilds@gmail.com>
2024-05-14 11:25:45 +0800

    remove address

--- ignition/modules/SetTokenCreator.ts
+++ ignition/modules/SetTokenCreator.ts
@@ -1,6 +1,6 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const CONTROLLER: string = "0xd5c077Efe284b060c68c4Fdf9888d4734DDe74E0";
+const CONTROLLER: string = "";
 
 const SetTokenCreator = buildModule("SetTokenCreator", (m) => {
   const controller = m.getParameter("Controller", CONTROLLER);
--- scripts/initController.ts
+++ scripts/initController.ts
@@ -1,14 +1,11 @@
 import { ethers } from "hardhat";
 async function main() {
   const [owner] = await ethers.getSigners();
-  const ControllerAddress = "0xd5c077Efe284b060c68c4Fdf9888d4734DDe74E0";
-  const IntegrationRegistryAddress =
-    "0x22199Fe14E098d726b75347d35Dd5E4AC71Af09d";
-  const SetTokenCreatorAddress = "0x4DBBc91D75b69865e61632DB8c5e36C544A3B6D9";
-  const BasicIssuanceModuleAddress =
-    "0x0Dd18972815D8A2b611F82F57E0bA26faaA972e1";
-  const AuctionRebalanceModuleAddress =
-    "0x9fab487f90c0A3b20717c245004952da9827C8Ff";
+  const ControllerAddress = "";
+  const IntegrationRegistryAddress = "";
+  const SetTokenCreatorAddress = "";
+  const BasicIssuanceModuleAddress = "";
+  const AuctionRebalanceModuleAddress = "";
 
   const INTEGRATION_REGISTRY_RESOURCE_ID = 0;
   const PRICE_ORACLE_RESOURCE_ID = 1;
